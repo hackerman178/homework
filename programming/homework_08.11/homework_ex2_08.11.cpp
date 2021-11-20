a8bc5db982347aa7752bc245cfd664162e13f038
hackerman178 <91876933+hackerman178@users.noreply.github.com>
2021-11-20 19:28:14 +0300

    Add files via upload

---
+++ programming/homework_08.11/homework_ex2_08.11.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+void shiftarray(int* A, int n, int k) {
+	int h = 0;
+	for (int i = n; i < n + k; ++i) {
+		A[i] = A[h]; 
+		A[h] = 0;
+		++h; 
+		if (h>k) break;
+	}
+}
+int main() {
+	int k, n;
+	std::cout << "input positive n = ";
+	std::cin >> n;
+	std::cout << "input positive k less than n = ";
+	std::cin >> k;
+	int* A = new int[n+k];
+	for (int i = 0; i < n; ++i) 
+		A[i] = i;
+	for (int i = n; i < n + k; ++i) A[i] = 0;
+	shiftarray(A, n, k);
+	for (int i = k; i < n + k; ++i)
+		std::cout << A[i] << " ";
+}
