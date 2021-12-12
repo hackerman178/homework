63a076d6d869ea0117db2ac3c1ba9b0e1c0636a1
hackerman178 <91876933+hackerman178@users.noreply.github.com>
2021-12-12 17:23:20 +0300

    Add files via upload

---
+++ programming/homework_25.10/homework_ex5_25.10.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+int main()
+{
+	const int n = 11;
+	int a[n] = { 1,11,7,9,8,6,0,2,10,3,5 };//массив от 0 до 11 с пропущенным символом
+	int  s2 = 0, s1 = (n * (n + 1)) / 2;
+	for (int i = 0; i < 11; ++i) s2 += a[i];
+	std::cout << s1 - s2;
+}
