463c8ab6143dcf8e5a8a2e3f06ec6c7cc05e0d52
hackerman178 <91876933+hackerman178@users.noreply.github.com>
2021-12-20 01:54:29 +0300

    Add files via upload

---
+++ programming/homework_08.11/homework_ex2_08.11.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+int main() {
+	int k, n;
+	std::cout << "input positive n = ";
+	do {
+		std::cin >> n;
+	} while (n < 1);
+	std::cout << "input k = ";
+	std::cin >> k;
+	if (k > 0) k %= n;
+	else k = n + k % n;
+	int* A = new int[n];
+	for (int i = 0; i < n; ++i)  A[i] = i;
+	
+	  for (int i = 0, j = k - 1; i < k / 2; --j, ++i) std::swap(A[i], A[j]);
+
+		for (int i = k, j=n-1; i<(n+k)/2; --j, ++i) std::swap(A[i], A[j]);
+
+			for (int i = 0, j=n-1; i < n/2; --j, ++i) std::swap(A[i], A[j]);
+
+	for (int i = 0; i < n; ++i)  std::cout << A[i] << " ";
+	return 0;
+}
